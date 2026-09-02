180af5875aea3b404739033fe2bab0432e97158b
Sabbir Ahmed Jisan <jisancse875@gmail.com>
2026-09-02 23:19:06 +0600

    Refactor functions in answer.js for correctness
    Fixed various issues in functions including fare calculation and verdict determination.

--- answer.js
+++ answer.js
@@ -43,37 +43,33 @@ function validateUsername(username){
     return "Available";
 }
 
-//Question 04
-function getCngFare(distance, isNight=false, waitingMinutes=0){
-    let fare=50;
-    if(distance>2){
-        fare=fare+(distance-2)*15;
-    }
-    fare=fare+waitingMinutes*2;
-    if(isNight){
-        fare=fare*1.2;
-    }
+// Question 04
+function getCngFare(distance, isNight = false, waitingMinutes=0) {
+  let fare=50;
+  if (distance>2){
+    fare+=(distance-2)*15;
+  }
+  fare+=waitingMinutes* 2;
+  if(isNight){
+    fare*=1.2;
+  }
+  return fare;
 }
 
-//Question 05
+// Question 05
 const getChaseVerdict=(target, scored, ballsLeft)=>{
-    const runsNeeded=target-scored;
-    if(runsNeeded<=0){
-        return "Won";
-    }
-    if(ballsLeft<=0){
-        return "Lost";
-    }
-    const requiredRate=(runsNeeded/ballsLeft)*6;
-    let verdict;
-    if(requiredRate<=6){
-        verdict="Comfortable";
-    }
-    else if(requiredRate<=12){
-        verdict="Tough";
-    }
-    else{
-        verdict="Almost Impossible";
-    }
-    return "Need "+runsNeeded+" runs in "+ballsLeft+" balls | "+verdict;
+  const runsNeeded=target-scored;
+  if(runsNeeded<=0)
+      return "Won";
+  if(ballsLeft<=0)
+      return "Lost";
+  const requiredRate=(runsNeeded/ballsLeft)*6;
+  let verdict;
+  if(requiredRate<= 6)
+      verdict="Comfortable";
+  else if(requiredRate<=12)
+      verdict="Tough";
+  else 
+      verdict="Almost Impossible";
+  return `Need ${runsNeeded} runs in ${ballsLeft} balls | ${verdict}`;
 }
